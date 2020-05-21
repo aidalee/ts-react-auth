53f22937fcec7453757c98d0129597ef451b01f8
lx <1475028433@qq.com>
2020-05-21 13:12:34 +0800

    ts-react实现认证系统

---
+++ src/App.tsx
@@ -0,0 +1,28 @@
+import React, { FC } from "react";
+import { Button } from "antd";
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <Button type="primary">lalal</Button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
