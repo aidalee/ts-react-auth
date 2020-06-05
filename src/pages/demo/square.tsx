762feb6437402566771e668d6d86f6580a3fcde9
x_li <lixueccf@outlook.com>
2020-06-05 23:13:24 +0800

    完成小游戏基本功能

--- src/pages/demo/square.tsx
+++ src/pages/demo/square.tsx
@@ -8,19 +8,27 @@ type StateType = {
   value: any;
 };
 // type NativeSquareProps = BaseSquareProps & DOMAttributes<HTMLElement>;
-class Square extends React.Component<BaseSquareProps, StateType> {
-  constructor(props: BaseSquareProps) {
-    super(props);
-    this.state = {
-      value: null,
-    };
-  }
-  render() {
-    return (
-      <button className="square" onClick={() => this.props.onClick()}>
-        {this.props.value}
-      </button>
-    );
-  }
+// class Square extends React.Component<BaseSquareProps, StateType> {
+//   constructor(props: BaseSquareProps) {
+//     super(props);
+//     this.state = {
+//       value: null,
+//     };
+//   }
+//   render() {
+//     return (
+//       <button className="square" onClick={() => this.props.onClick()}>
+//         {this.props.value}
+//       </button>
+//     );
+//   }
+// }
+// 函数式组件的形式
+function Square(props: BaseSquareProps) {
+  return (
+    <button className="square" onClick={props.onClick}>
+      {props.value}
+    </button>
+  );
 }
 export default Square;
